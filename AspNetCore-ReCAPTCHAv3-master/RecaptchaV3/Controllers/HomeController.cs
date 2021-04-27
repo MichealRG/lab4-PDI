02e2b83056cf79ed3e7a12d32e1991cfee6fc86f
Michał <KrzyzowskiM1998@gmail.com>
2021-04-27 14:01:55 +0200

    ff

---
+++ AspNetCore-ReCAPTCHAv3-master/RecaptchaV3/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using RecaptchaV3.Extensions;
+using RecaptchaV3.Models;
+using RecaptchaV3.Services;
+
+namespace RecaptchaV3.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRecaptchaExtension _recaptcha;
+        public HomeController(IRecaptchaExtension recaptcha)
+        {
+            _recaptcha = recaptcha;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            if (ModelState.IsValid && UserService.IsValid(user))
+            {
+                return RedirectToAction("Welcome", "Home");
+            }
+            return View();
+        }
+
+        public IActionResult Welcome()
+        {
+            ViewData["Message"] = $"You are online now! Date:{DateTime.Now.ToShortTimeString()}";
+            return View();
+        }
+        public IActionResult Error()
+        {
+            ViewBag.ErrorMessage = "There is an error!";
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> Verify(string token)
+        {
+            var verified = await _recaptcha.VerifyAsync(token);
+
+            return Json(verified);
+        }
+    }
+}
